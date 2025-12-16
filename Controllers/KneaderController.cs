d1c8b565ea57c8b6787c5e452b292a847ce6f38e
lorlando <lorlando@RMX-FJMS624>
2025-12-16 16:19:36 -0600

    The end date filter was deleted

--- Controllers/KneaderController.cs
+++ Controllers/KneaderController.cs
@@ -26,38 +26,31 @@ public class KneaderController : Controller
     }
 
     [HttpGet]
-    public JsonResult GetData(DateTime fechaInicio, DateTime fechaFin, string kneader, string turno)
+    public JsonResult GetData(DateTime fecha, string kneader, string turno)
     {
-        // Base query por fechas y kneader
         var query = _context.KneaderM
-                            .Where(x => x.Date.Date >= fechaInicio.Date &&
-                                        x.Date.Date <= fechaFin.Date &&
-                                        x.Kneader == kneader);
+                            .Where(x => x.Kneader == kneader);
 
-        // Filtrar por turno si se especifica
-        if (!string.IsNullOrEmpty(turno))
+        switch (turno)
         {
-            switch (turno)
-            {
-                case "Turno1": // 07:00 - 14:59
-                    query = query.Where(x => x.Time >= new TimeSpan(07, 0, 1) &&
-                                             x.Time < new TimeSpan(14, 59, 59));
-                    break;
+            case "Turno1": // 07:00 - 14:59
+                query = query.Where(x => x.Date.Date == fecha.Date &&
+                                         x.Time >= new TimeSpan(7, 0, 0) &&
+                                         x.Time <= new TimeSpan(14, 59, 59));
+                break;
 
-                case "Turno2": // 15:00 - 23:59
-                    query = query.Where(x => x.Time >= new TimeSpan(15, 0, 1) &&
-                                             x.Time < new TimeSpan(23, 59, 59));
-                    break;
+            case "Turno2": // 15:00 - 23:59
+                query = query.Where(x => x.Date.Date == fecha.Date &&
+                                         x.Time >= new TimeSpan(15, 0, 0) &&
+                                         x.Time <= new TimeSpan(23, 59, 59));
+                break;
 
-                case "Turno3":
-                    // 00:00:01 - 06:59:59 del día siguiente
-                    var siguienteDia = fechaInicio.Date.AddDays(1);
-
-                    query = query.Where(x => x.Date == siguienteDia &&
-                                             x.Time >= new TimeSpan(0, 0, 1) &&
-                                             x.Time <= new TimeSpan(6, 59, 59));
-                    break;
-            }
+            case "Turno3": // 00:00 - 06:59 del día siguiente
+                var siguienteDia = fecha.Date.AddDays(0);
+                query = query.Where(x => x.Date.Date == siguienteDia &&
+                                         x.Time >= new TimeSpan(0, 0, 1) &&
+                                         x.Time <= new TimeSpan(6, 59, 59));
+                break;
         }
 
         var datos = query.OrderBy(x => x.Date)
